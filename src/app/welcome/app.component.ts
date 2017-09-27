aa3a604fe711ce742ff7ac976670a653390271b8
jellyforce <jelly.wie@gmail.com>
2017-09-27 13:59:13 +0200

    ASDFRTGYUIO

---
+++ src/app/welcome/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  /***/
+  selector: 'app-root',
+  /** TemplateUrl: external markup for the page*/
+  templateUrl: './app.component.html',
+  /** StyleUrls: external styling*/
+  styleUrls: ['./app.component.css']
+})
+/** Export: Making this class available for use somewhere else*/
+export class AppComponent {
+  /** Attributes: serve as data, these can be used as {{title}} in your html document*/
+  title = 'TMM';
+}
--- src/app/welcome/app.module.ts
+++ src/app/welcome/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  /** Declarations: make these components usable by other parts of the app*/
   declarations: [
     AppComponent
   ],
+  /** Imports: these modules are used by this module*/
   imports: [
     BrowserModule
   ],
+  /** Providers: */
   providers: [],
+  /**Bootstrap: */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
